e539ee107677c4add8bc43c6b1f998572d8f1d3f
Phil Plencner <philip_plencner@harvard.edu>
2023-06-14 13:20:28 -0400

    Moving analytics tags to mirador.js in mps-viewer.

--- src/plugins/MiradorHarvardAnalytics.js
+++ src/plugins/MiradorHarvardAnalytics.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import $ from "jquery";
 
+const mapStateToProps = (state, { windowId, containerId }) => ({
+  scriptTag: state.config.miradorAnalyticsPlugin.scriptTag,
+  noscriptTag: state.config.miradorAnalyticsPlugin.noscriptTag,
+});
+
 class harvardAnalytics extends Component {
   render() {
 
@@ -10,11 +15,13 @@ class harvardAnalytics extends Component {
   }
 
   componentDidMount() {
+    const { scriptTag, noscriptTag } = this.props;
+
     $("head").prepend(
-      "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WSHSDQ5');</script><!-- End Google Tag Manager -->"
+      scriptTag
     );
     $("body").prepend(
-      '<!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSHSDQ5"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->'
+      noscriptTag
     );
   }
 }
@@ -23,4 +30,5 @@ export default {
   target: 'Window',
   mode: 'add',
   component: harvardAnalytics,
+  mapStateToProps,
 }
